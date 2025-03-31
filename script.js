97618d63c3d6a9d4298603fe91521259b8d6fd02
yhkim <148407209+honeyprofit@users.noreply.github.com>
2025-03-31 21:57:21 +0900

    Update script.js

--- script.js
+++ script.js
@@ -82,7 +82,13 @@ class App {
   #workouts = [];
 
   constructor() {
+    // Get user's position
     this._getPosition();
+
+    // Get data from local storage
+    this._getLocalStorage();
+
+    // Attach event handlers
     form.addEventListener('submit', this._newWorkout.bind(this));
     inputType.addEventListener('change', this._toggleElevationField);
     containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
@@ -209,6 +215,9 @@ class App {
 
     // Hide form + clear input field
     this._hideForm();
+
+    //Set local storage to all workouts
+    this._setLocalStorage();
   }
 
   _renderWorkoutMarker(workout) {
@@ -299,6 +308,15 @@ class App {
 
     //using the public interface
     workout.click();
+  }
+
+  _setLocalStorage() {
+    localStorage.setItem('workouts', JSON.stringify(this.#workouts));
+  }
+
+  _getLocalStorage() {
+    const data = JSON.parse(localStorage.getItem('workouts'));
+    console.log(data);
   }
 }
 
